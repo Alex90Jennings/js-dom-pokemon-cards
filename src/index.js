c959e95f426d8cdab0c52381b11f3a702c684c1d
Alex Jennings <aj0312@my.bristol.ac.uk>
2022-04-19 19:14:07 +0200

    core criteria complete, minus formatting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
-
 console.log(data);
 
-//You can start simple and just render a single 
-//pokemon card from the first element
+const cardListUL = document.getElementById("card-list");
+for (let i = 0; i < data.length; i++) {
+  const pokemon = data[i];
+
+  const cardLI = document.createElement("li");
+  cardLI.setAttribute("class", "card");
+  cardListUL.append(cardLI);
+
+  const cardH2 = document.createElement("h2");
+  cardH2.innerHTML = pokemon.name;
+  cardLI.append(cardH2);
+
+  const cardIMG = document.createElement("img");
+  cardIMG.setAttribute("class", "card--img");
+  cardIMG.width = "256";
+  cardIMG.src = pokemon.sprites.other["official-artwork"].front_default;
+  cardLI.append(cardIMG);
+
+  const statsUL = document.createElement("ul");
+  statsUL.setAttribute("class", "card--text");
+  cardLI.append(statsUL);
+
+  const statsArray = pokemon.stats;
+  for (let j = 0; j < statsArray.length; j++) {
+    const statLI = document.createElement("li");
+    statLI.innerHTML = statsArray[j].stat.name + ": " + statsArray[j].base_stat;
+    statsUL.append(statLI);
+  }
+}
 console.log(data[0]);
